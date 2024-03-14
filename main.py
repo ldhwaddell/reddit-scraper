446049b747efea2d23789f17e8192bfbe595269c
ldhwaddell <ldhwaddell@gmail.com>
2024-03-13 22:45:20 -0300

    add main with examples

---
+++ main.py
@@ -0,0 +1,13 @@
+from reddit_scraper.reddit_scraper import RedditScraper
+
+
+if __name__ == "__main__":
+    url = "https://www.reddit.com/r/NovaScotia/rising"
+
+    # Instantiate a headless scraper with at most 6 threads
+    with RedditScraper(url=url, headless=True, max_workers=6) as rs:
+
+        # Scrape one post, with up to 10 comments, and download any images to a directory called test
+        posts = rs.get_posts(limit=1, comment_limit=10, download_images_dir="./test")
+
+    print(posts)
