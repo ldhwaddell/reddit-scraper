31c59b94c15152ee4106bda619f0b3b6bd35cd2c
ldhwaddell <ldhwaddell@gmail.com>
2024-03-14 23:37:24 -0300

    update args

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     with RedditScraper(url=url, headless=True, max_workers=6) as rs:
 
         # Scrape one post, with up to 10 comments, and download any images to a directory called test
-        posts = rs.get_posts(limit=1, comment_limit=10, download_images_dir="./test")
+        posts = rs.get_posts(limit=5, comment_limit=10, download_media_dir="./test")
 
     print(posts)
